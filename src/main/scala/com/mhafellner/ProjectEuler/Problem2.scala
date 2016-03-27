73b747b4a288b9a57ced02a98bb52b85750c51d9
Markus Hafellner <markus.hafellner@gmail.com>
2016-03-27 23:44:30 +0200

    TDD: Add tests

--- src/main/scala/com/mhafellner/ProjectEuler/Problem2.scala
+++ src/main/scala/com/mhafellner/ProjectEuler/Problem2.scala
@@ -4,5 +4,13 @@ package com.mhafellner.ProjectEuler
   * Created by Markus on 27/03/2016.
   */
 object Problem2 {
+  def main(args: Array[String]): Unit = {
+    val allEvenFibonaccisSum = addAllEvenFibonaccis(10)
+    println(allEvenFibonaccisSum)
+    allEvenFibonaccisSum
+  }
 
+  def addAllEvenFibonaccis(limit: Int): Int = {
+    limit
+  }
 }
--- src/test/scala/com/mhafellner/ProjectEuler/Problem2Spec.scala
+++ src/test/scala/com/mhafellner/ProjectEuler/Problem2Spec.scala
@@ -1,8 +1,23 @@
 package com.mhafellner.ProjectEuler
 
+import org.scalatest._
+
 /**
   * Created by Markus on 27/03/2016.
   */
-class Problem2Spec {
+class Problem2Spec extends FlatSpec with Matchers {
+  behavior of "Problem2"
+
+  "main" should "print the solution and not return anything" in {
+    Problem2.main(Array[String]()) should be()
+  }
+
+  "addAllEvenFibonaccis with limit 10" should "return 10" in {
+    Problem2.addAllEvenFibonaccis(10) should be(10)
+  }
+
+  "addAllEvenFibonaccis" should "return the sum of all even Fibonacci terms within the given limit" in {
+    Problem2.addAllEvenFibonaccis(10) should be(10)
+  }
 
 }
